3ae721592faa46c4252059dca51a1b0915771554
Samuel Lauan <samuelstudy893@gmail.com>
2026-03-11 23:48:48 -0300

    ui: update navigation layout and finalize theme toggle logic

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,6 +1,6 @@
 // Toggle Light/Dark Mode
 export function toggleMode() {
-  const button = document.querySelector('.theme')
+  const button = document.querySelector('#switch')
 
   button.addEventListener('click', () => {
     document.documentElement.classList.toggle("light")
